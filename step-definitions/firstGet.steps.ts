c3199991cf168fc0129f6e06f90c199506abfe34
Robchiiks <roberts.binovskis@magebit.com>
2020-09-02 17:28:06 +0300

    updates

--- step-definitions/firstGet.steps.ts
+++ step-definitions/firstGet.steps.ts
@@ -1,39 +1,22 @@
 import { binding, given, then, when} from 'cucumber-tsflow';
 import {assert} from 'chai';
-import fetch from 'node-fetch';
 import {CookieJar} from 'tough-cookie';
 import got from 'got';
-import {cookie} from 'request';
 import util from 'util';
-
 
 @binding()
 export class CookieSteps {
-  
-  
-  // public testCookies = new CookieJar();
   public website: string= 'https://www.github.com';
   public cookie1: string = '';
   public cookie2: string = '';
   public cookie3: string = '';
-  public assertCookieJar: string[] = new Array();
-
+  public cleanCookieJar: string[] = new Array();
   @given(/A set of three cookies/)  
-  public GetThoseThreeCookies() {    
-      // //var website = 'https://www.github.com';
-      // const cookieJar = new CookieJar();
-      // const setCookie = util.promisify(cookieJar.setCookie.bind(cookieJar));      
-      // setCookie('test1=firstTest', this.website);
-      // setCookie('test2=secondTest', this.website);
-      // setCookie('test3=thirdTest', this.website);
-      // got(this.website, {cookieJar});
-      // console.log(cookieJar.getCookies(this.website))  
+  public GetThoseThreeCookies() {
        this.cookie1 = 'test1=firstTest';
       this.cookie2 = 'test2=secondTest';
        this.cookie3 = 'test3=thirdTest';
-    return;
   }
-
   @when(/Making a GET call/)
   public MakingGETcall() { 
     (async () => {
@@ -43,24 +26,16 @@ export class CookieSteps {
     setCookie(this.cookie2, this.website);
     setCookie(this.cookie3, this.website);
     got(this.website, {cookieJar});
-    console.log(cookieJar.getCookiesSync(this.website));
-    console.log('breajk')
-    console.log(typeof cookieJar.getCookiesSync(this.website));
-    // this.assertCookieJar.push.apply(this.assertCookieJar, cookieJar);
-    // Array.prototype.push.apply(this.assertCookieJar, cookieJar);
-    this.assertCookieJar = Object.assign(this.assertCookieJar, cookieJar);
-    console.log('TEST')   
-    // console.log(this.assertCookieJar);
-    return;
+    this.cleanCookieJar = Object.assign(this.cleanCookieJar, cookieJar);
   })();
    
   }
   @then(/expect to have set of three cookies added/)
   public assertCookiesAdded() {
-    console.log('teeeeest')
-    // delete this.assertCookieJar['test1'];
-    console.log(clean(this.assertCookieJar,"test1"))
-    
+    let assertCookieJar: string[] = new Array();
+    clean(this.cleanCookieJar,"test1")
+     assertCookieJar = Object.assign(assertCookieJar, this.cleanCookieJar)
+     assert.notInclude(assertCookieJar, 'test1', "cookie deleted");
   }
 }
 
